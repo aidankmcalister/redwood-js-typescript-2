4ecf2453a7beea82b40ad9a9797b133a602fe848
Aidan <aidankmcalister@gmail.com>
2024-01-15 19:17:33 -0600

    create comment

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -1,5 +1,5 @@
 import { Link, routes } from '@redwoodjs/router'
-
+import CommentsCell from 'src/components/CommentsCell'
 import type { Post } from 'types/graphql'
 
 const truncate = (text: string, length: number) => {
@@ -15,13 +15,18 @@ const Article = ({ article, summary = false }: Props) => {
   return (
     <article className="mt-10">
       <header>
-        <h2 className="text-xl text-blue-700 font-semibold">
+        <h2 className="text-xl font-semibold text-blue-700">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div className="mt-2 text-gray-900 font-light">
+      <div className="mt-2 font-light text-gray-900">
         {summary ? truncate(article.body, 100) : article.body}
       </div>
+      {!summary && (
+        <div className="mt-12">
+          <CommentsCell />
+        </div>
+      )}
     </article>
   )
 }
